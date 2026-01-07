e4d93181a9416ab02726d40f255ec3207d39c9bd
Tushar3529-dev <tusharyadav3529@gmail.com>
2026-01-07 20:12:56 +0530

    some functions in events

--- 01_event.js
+++ 01_event.js
@@ -1,4 +1,25 @@
 
+const EventEmitter = require('events');
+
+const myEmitter = new EventEmitter();
+
+                    
+
+myEmitter.on("name",()=>{
+    console.log("My name is John Doe");
+})
+
+
+myEmitter.on("name",(yourName)=>{
+    console.log(`My name is ${yourName}`);
+})
+
+
+
+
+
+myEmitter.emit("name");
+
 
 
 /* EventEmitter → stores events
@@ -6,3 +27,16 @@
 .emit() → announces event
 Event Loop → executes listener code
  */
+
+
+myEmitter.on("age",(age)=>{
+    console.log(`Your age is : ${age}`);
+   
+})
+
+  myEmitter.emit("age",25);
+
+myEmitter.emit("name",15);
+myEmitter.removeAllListeners("name");
+myEmitter.emit("name"); // No output since listeners are removed
+  myEmitter.emit("age",25); 
