6c5424d5ca1034ef6ce481354d9ae93ebcf05958
Tushar3529-dev <tusharyadav3529@gmail.com>
2026-01-08 17:58:33 +0530

    created custom class and extended the class imported from the require(events)

--- 01_event.js
+++ 01_event.js
@@ -1,41 +1,24 @@
 const EventEmitter = require('events');
 
-const myEmitter = new EventEmitter();
+class MyEmitter extends EventEmitter{
 
-function showName(name){
-console.log(`Your Name is : ${name}`);
+sendsMessages(msg){
+    console.log(`Your Message is Sent : ${msg}`);
+    this.emit('messageRecived', msg);
+    
 }
 
-function showAge(age){
-console.log(`Your Age is : ${age}`);
+
+
+
+
 }
 
+const chat= new MyEmitter();
 
-myEmitter.on('userInfo',showName);
-myEmitter.on('userInfo',showAge);
+chat.on('messageRecived',(msg)=>{
+    console.log(`Recived Message: ${msg}`);
+});
 
-myEmitter.emit('userInfo','John Doe');
-myEmitter.emit('userInfo',30);
-
-
-
-// Output:
-/*
- Your Name is : John Doe
-Your Age is : John Doe
-Your Name is : 30
-Your Age is : 30
- */
-
-// We will remove the showname Listener so only your age is : ${value} will be printed
-
-myEmitter.off('userInfo',showName);
-myEmitter.emit('userInfo','John Doe');
-myEmitter.emit('userInfo',30);
-
-
-// OutPut 
-/*
- Your Age is : John Doe
-Your Age is : 30
- */
+chat.sendsMessages('Hello, World!');
+chat.sendsMessages('This is a test message.');
